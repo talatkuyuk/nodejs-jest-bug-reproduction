aa91d980711ba68b0fcdea76914d397ab8ae7264
“talatkuyuk” <talatkuyuk@gmail.com>
2021-09-20 08:11:23 +0300

    added bug reproduction code

---
+++ test.js
@@ -0,0 +1,39 @@
+const httpMocks = require('node-mocks-http');
+
+class ApiError extends Error {
+    constructor(message) {
+        super(message);
+        this.name = this.constructor.name;
+    }    
+}
+
+const auth = async (req, res, next) => {
+	return new Promise((resolve, reject) => {
+		if (!req.header('Authorization'))
+			reject(new Error("No Auth Token"));
+		resolve();
+	})
+	.then(() => next())
+	.catch((error) => {next(error)});
+};
+
+
+test('should return 401 if Authorization Header is absent', async () => {
+	const req = httpMocks.createRequest();
+	const res = httpMocks.createResponse();
+	const next = jest.fn();
+
+	// I did't set the Authorization Header of the request intentionally, 
+	// in order the Auth function to catch the error and call the next(error)
+
+	await auth(req, res, next);
+
+	const expectedError = new ApiError("No Auth Token");
+
+	expect(next).toHaveBeenCalledWith(expectedError);
+	// IT PASSED ! THIS IS A PROBLEM !
+	// Actually, it should have been like below: 
+	// Expected: [ApiError: No Auth Token]
+    // Received: [Error: No Auth token]
+});
+
